b4c8778ecb70cf18eee8b952224fd19f8830cf19
Alifoxy <nightfoxy666@gmail.com>
2022-11-01 16:55:42 +0300

    commit2

--- main_js.js
+++ main_js.js
@@ -1,1 +1,52 @@
 "use strict";
+
+let s = 'hello';
+console.log(s);
+s = 'owu';
+console.log(s);
+s = 'com';
+console.log(s);
+s = 'ua';
+console.log(s);
+
+let num1 = 10;
+console.log(num1);
+num1 = -999;
+console.log(num1);
+num1 = 123;
+console.log(num1);
+
+const A1 = 3.14;
+console.log(A1);
+
+const B1 = 2.7;
+console.log(B1);
+
+const C1 = 16;
+console.log(C1);
+
+let e = 6 > 7;
+console.log(e);
+e = 7 === 7;
+console.log(e);
+
+let firstName = 'Shabanova';
+let middleName = 'Inna';
+let lastName = 'Olegivna';
+let person = `${firstName} ${middleName} ${lastName}`;
+console.log(person);
+
+let a = 100;
+console.log(typeof a);
+
+let b = '100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c);
+
+
+
+
+
+
