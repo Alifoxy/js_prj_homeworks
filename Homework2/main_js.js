0ba1d0d94519ba67dfaa5b0ef9d99923f7dd9496
Alifoxy <nightfoxy666@gmail.com>
2022-11-03 17:49:53 +0300

    commit4

---
+++ Homework2/main_js.js
@@ -0,0 +1,95 @@
+"use strict";
+
+let forestAnim = ['wolf','fox','deer','bear','boar','raccoon','squirrel','moose','owl','woodpecker'];
+console.log(forestAnim[0]);
+console.log(forestAnim[1]);
+console.log(forestAnim[2]);
+console.log(forestAnim[3]);
+console.log(forestAnim[4]);
+console.log(forestAnim[5]);
+console.log(forestAnim[6]);
+console.log(forestAnim[7]);
+console.log(forestAnim[8]);
+console.log(forestAnim[9]);
+
+let book1 = {
+    title: 'Misery' ,
+    pageCount: 370,
+    genre: 'horror'
+}
+console.log(book1)
+
+let book2 = {
+    title: 'The Hound of the Baskervilles' ,
+    pageCount: 224,
+    genre: 'detective'
+}
+console.log(book2)
+
+let book3 = {
+    title: 'To Kill a Mockingbird' ,
+    pageCount: 336,
+    genre: 'novel'
+}
+console.log(book3)
+
+let book4 = {
+    title: 'It' ,
+    pageCount: 1116,
+    genre: 'horror',
+    authors:[
+        {name:'stephen king', age:'75'}
+    ]
+}
+console.log(book4.authors)
+
+let book5 = {
+    title: 'It' ,
+    pageCount: 1116,
+    genre: 'horror',
+    authors:[
+        {name:'Stephen King', age:'75'}
+    ]
+}
+console.log(book5.authors)
+
+let book6 = {
+    title: 'Gone with the Wind' ,
+    pageCount: 1037,
+    genre: 'fiction',
+    authors:[
+        {name:'Margaret Mitchell', age:'49'}
+    ]
+}
+console.log(book6.authors)
+
+let user = [
+    {name:'Olga', username:'olichka', password:3452},
+    {name:'Boris', username:'borya', password:4413},
+    {name:'Maria', username:'mimi', password:3659},
+    {name:'Volodymyr', username:'vova', password:3371},
+    {name:'Katherina', username:'katie', password:6478},
+    {name:'Dmytro', username:'dimix', password:2412},
+    {name:'Oleg', username:'olegator', password:8475},
+    {name:'Anastasia', username:'nastinka', password:3913},
+    {name:'Veronika', username:'verona', password:1956},
+    {name:'Oleksandr', username:'sanik', password:5142},
+]
+
+console.log(user[0].password);
+console.log(user[1].password);
+console.log(user[2].password);
+console.log(user[3].password);
+console.log(user[4].password);
+console.log(user[5].password);
+console.log(user[6].password);
+console.log(user[7].password);
+console.log(user[8].password);
+console.log(user[9].password);
+
+
+
+
+
+
+
